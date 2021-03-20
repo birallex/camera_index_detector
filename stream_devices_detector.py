ca3581f1d0785966f388bac2bbbb671d5a1bcfbc
birallex <biralex204@gmail.com>
2021-03-20 22:40:39 +0300

    Add more functionality(range to search) in devices_detector

--- stream_devices_detector.py
+++ stream_devices_detector.py
@@ -1,9 +1,9 @@
 import cv2
 
 
-def get_video_devices(max_device_number = 20):
+def get_video_devices(min_device_number = 0, max_device_number = 20):
     list_of_founded_devices = []
-    for device_number in range(0, max_device_number):
+    for device_number in range(min_device_number, max_device_number):
         cap = cv2.VideoCapture(device_number)
         if cap is None or not cap.isOpened():
             pass
